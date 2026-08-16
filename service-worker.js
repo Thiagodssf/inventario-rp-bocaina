5cf1cf2e2c26f8f39e2c6a87ed51ad22f3c046b6
Thiagodssf <65789820+Thiagodssf@users.noreply.github.com>
2026-08-16 18:41:04 -0300

    Atualiza cache para exibir total mensal

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='bocaina-rp-v14';
-const ASSETS=['./','./index.html','./styles.css','./header-overrides.css','./script.js','./stock.js','./menu-overrides.js','./municiamento-banco.js','./cardapio.js?v=14','./banco-generos-ajuste.js','./planejamento-semanal.js?v=14','./manifest.webmanifest','./brasao-bocaina.png'];
+const CACHE='bocaina-rp-v16';
+const ASSETS=['./','./index.html','./styles.css','./header-overrides.css','./script.js','./stock.js','./menu-overrides.js','./municiamento-banco.js','./cardapio.js?v=15','./banco-generos-ajuste.js','./planejamento-semanal.js?v=15','./edit-cardapio-override.js?v=15','./manifest.webmanifest','./brasao-bocaina.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request))));
