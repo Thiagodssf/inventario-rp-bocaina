378ce4d7e7dc243189c7b1eefb5b65c70ca37e10
Thiagodssf <65789820+Thiagodssf@users.noreply.github.com>
2026-08-16 17:38:23 -0300

    Atualiza cache para publicar a opção de tipo de refeição

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='bocaina-rp-v12';
+const CACHE='bocaina-rp-v13';
 const ASSETS=['./','./index.html','./styles.css','./header-overrides.css','./script.js','./stock.js','./menu-overrides.js','./municiamento-banco.js','./cardapio.js','./banco-generos-ajuste.js','./planejamento-semanal.js','./manifest.webmanifest','./brasao-bocaina.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
