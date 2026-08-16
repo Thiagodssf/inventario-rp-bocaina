a809ec741a4532b20eecb24e087d4505980be5f7
Thiagodssf <65789820+Thiagodssf@users.noreply.github.com>
2026-08-16 16:59:43 -0300

    Atualiza cache para banco de gêneros

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='bocaina-rp-v10';
-const ASSETS=['./','./index.html','./styles.css','./header-overrides.css','./script.js','./stock.js','./menu-overrides.js','./manifest.webmanifest','./brasao-bocaina.png'];
+const CACHE='bocaina-rp-v11';
+const ASSETS=['./','./index.html','./styles.css','./header-overrides.css','./script.js','./stock.js','./menu-overrides.js','./municiamento-banco.js','./manifest.webmanifest','./brasao-bocaina.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request))));
